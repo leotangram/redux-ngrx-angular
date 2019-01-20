8720b2582ce146a80147ba0dd4ab575f2933c303
Leonardo Omana <leotangram@gmail.com>
2019-01-20 14:34:19 -0500

    Basic actions and reducers

---
+++ 01-redux-basic/app-1.ts
@@ -0,0 +1,23 @@
+// Acciones
+interface Action {
+  type: string
+  payload?: any
+}
+
+// const incrementadorAction: Action = {
+//   type: 'INCREMENTAR'
+// }
+
+function reducer(state = 10, action: Action) {
+  if (action.type === 'INCREMENTAR') {
+    return state += 1
+  }
+  return state
+}
+
+// Usar el Reducer
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+}
+
+console.log(reducer(10, incrementadorAction))
