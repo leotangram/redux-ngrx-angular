92271bba5f9b9926a14146d20dea2c92307ef3df
Leonardo Omana <leotangram@gmail.com>
2019-01-20 15:02:22 -0500

    Cofig actions in a reducer

--- 01-redux-basic/app-1.ts
+++ 01-redux-basic/app-1.ts
@@ -9,15 +9,41 @@ interface Action {
 // }
 
 function reducer(state = 10, action: Action) {
-  if (action.type === 'INCREMENTAR') {
-    return state += 1
+
+  switch (action.type) {
+    case 'INCREMENTAR':
+      return state += 1
+    case 'DECREMENTAR':
+      return state -= 1
+    case 'MULTIPLICAR':
+      return state * action.payload
+    case 'DIVIDIR':
+      return state / action.payload
+    default:
+      return state
   }
-  return state
+
 }
 
 // Usar el Reducer
 const incrementadorAction: Action = {
   type: 'INCREMENTAR'
 }
+console.log(reducer(10, incrementadorAction)) // 11
 
-console.log(reducer(10, incrementadorAction))
+const decrementadorAction: Action = {
+  type: 'DECREMENTAR'
+}
+console.log(reducer(10, decrementadorAction)) // 9
+
+const multiplicarAction: Action = {
+  type: 'MULTIPLICAR',
+  payload: 2
+}
+console.log(reducer(10, multiplicarAction)) // 9
+
+const dividirAction: Action = {
+  type: 'DIVIDIR',
+  payload: 2
+}
+console.log(reducer(10, dividirAction)) // 5
