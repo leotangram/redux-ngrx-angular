101c361b8a6a164ad754e7b088b322c8f72cbc6a
Leonardo Omana <leotangram@gmail.com>
2019-01-21 21:10:12 -0500

    Listener specific changes at state

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -17,9 +17,8 @@ export class AppComponent {
   constructor(
     private store: Store<AppState>
   ) {
-    this.store.subscribe( state => {
-      this.contador = state.contador
-    })
+    this.store.select('contador')
+      .subscribe( contador => this.contador = contador)
   }
 
   incrementar() {
