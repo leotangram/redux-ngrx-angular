ff7ff13ffc6ddcc9e70125ea31228d9cc734be3b
Leonardo Omana <leotangram@gmail.com>
2019-01-21 21:04:07 -0500

    Store Devtools

--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 // ngrx
 import { StoreModule } from '@ngrx/store';
 import { contadorReducer } from './contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,11 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ contador: contadorReducer })
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 5,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
