bae44921b7f7ec2c287deefd65446dc9b66bf81b
Leonardo Omana <leotangram@gmail.com>
2019-01-26 16:22:26 -0500

    Pendent Todos count

--- 03-todoapp/src/app/todo/todo-footer/todo-footer.component.ts
+++ 03-todoapp/src/app/todo/todo-footer/todo-footer.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import * as fromFiltro from '../../filter/filter.actions'
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducers';
+import { Todo } from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-footer',
@@ -13,11 +14,13 @@ export class TodoFooterComponent implements OnInit {
 
   filtrosValidos: fromFiltro.filtrosValidos[] = ['todos', 'completados', 'pendientes']
   filtroActual: fromFiltro.filtrosValidos
+  pendientes: number
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.store.subscribe(state => {
+      this.contarPendientes(state.todos)
       this.filtroActual = state.filtro
     })
   }
@@ -25,6 +28,10 @@ export class TodoFooterComponent implements OnInit {
   cambiarFiltro(nuevoFiltro: fromFiltro.filtrosValidos) {
     const accion = new fromFiltro.SetFiltroAction(nuevoFiltro)
     this.store.dispatch(accion)
+  }
+
+  contarPendientes(todos: Todo[]) {
+    this.pendientes = todos.filter(todo => !todo.completado).length
   }
 
 }
